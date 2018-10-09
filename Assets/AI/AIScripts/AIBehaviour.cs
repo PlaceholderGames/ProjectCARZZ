7ba69c8faac07e7b3f874b4792a8cc670ed25d97
Gerencser S (FCES) <17089409@glam.ac.uk>
2018-10-09 16:05:16 +0100

    Added a basic AI behaviour and collision script

---
+++ Assets/AI/AIScripts/AIBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIBehaviour : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public Transform target;
+    public float moveSpeed = 5f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce((target.position.x - transform.position.x) * moveSpeed * Time.deltaTime, 0, (target.position.z - transform.position.z) * moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
+
+    }
+}
---
+++ Assets/AI/AIScripts/AICollision.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class AICollision : MonoBehaviour
+{
+    public GameObject ai;
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.tag);
+        if (collisionInfo.collider.tag == "target")
+            Destroy(ai, 5);
+    }
+
+}

6f7e40c6bbb2aaaab567a13ef3b62c0ff2688634
Gerencser S (FCES) <17089409@glam.ac.uk>
2018-10-16 16:46:33 +0100

    Limited the AI's tracking to a set distance

--- Assets/AI/AIScripts/AIBehaviour.cs
+++ Assets/AI/AIScripts/AIBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,11 +9,19 @@ public class AIBehaviour : MonoBehaviour
     public Rigidbody rb;
     public Transform target;
     public float moveSpeed = 5f;
+    public float detectDistance= 200f;  //the distance in which the AI can follow the player
+    float xpos, zpos;
+    
 
-    // Update is called once per frame
     void FixedUpdate()
+
     {
-        rb.AddForce((target.position.x - transform.position.x) * moveSpeed * Time.deltaTime, 0, (target.position.z - transform.position.z) * moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        xpos = (target.position.x - transform.position.x);
+        zpos = (target.position.z - transform.position.z);
+        //if the player is inside the detectable distance
+        if (Math.Abs(xpos) < detectDistance && Math.Abs(zpos)< detectDistance) 
+        //The AI gets the position of the player and it goes towards it.
+            rb.AddForce(Math.Max(xpos * moveSpeed,moveSpeed) * Time.deltaTime, 0, Math.Max(zpos * moveSpeed,moveSpeed) * Time.deltaTime, ForceMode.VelocityChange);
 
     }
 }

fc99235bae5b95661623b205e46b21a8b24116a0
DewyHealy <dewyhealy@yahoo.co.uk>
2018-11-04 21:36:27 +0000

    options menu
    spent some time on formatting and had to redo the menu a couple of times to get it working properly. it is really finnicky so it has taken me a while. I've still got some stuff to polishj but basically the video menu is done, i've just got some errors with this json implementation.

---
+++ Assets/Menu/Scripts/SettingsManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class SettingsManager : MonoBehaviour {
+
+    public Toggle fullscreenToggle; // initialise UI elements
+    public Dropdown resolutionDropdown;
+    public Dropdown textureQualityDropdown;
+    public Dropdown antialisingDropdown;
+    public Dropdown vSyncDropdown;
+    public Slider musicVolumeSlider;
+    public Button applyVideoButton;
+
+    public AudioSource musicSource;
+    public Resolution[] resolutions;
+    public GameSettings gameSettings;
+
+    void OnEnable()
+    {
+        gameSettings = new GameSettings();
+
+        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        antialisingDropdown.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
+        vSyncDropdown.onValueChanged.AddListener(delegate { OnVSyncChange(); });
+        musicVolumeSlider.onValueChanged.AddListener(delegate { onMusicVolumeChange(); });
+        applyVideoButton.onClick.AddListener(delegate { onVideoButtonClick(); });
+
+
+        resolutions = Screen.resolutions; // getting an array of resolutions
+        foreach(Resolution resolution in resolutions)
+            {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+
+        LoadSettings();
+    }
+
+    public void OnFullscreenToggle()
+    {
+      gameSettings.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+
+    }
+    public void OnResolutionChange()
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+        gameSettings.resolutionIndex = resolutionDropdown.value;
+    }
+    public void OnTextureQualityChange()
+    {
+        QualitySettings.masterTextureLimit = gameSettings.textureQuality = textureQualityDropdown.value;
+    }
+    public void OnAntialiasingChange()
+    {
+        QualitySettings.antiAliasing = gameSettings.antialiasing = (int)Mathf.Pow(2, antialisingDropdown.value);
+    }
+    public void OnVSyncChange()
+    {
+        QualitySettings.vSyncCount = gameSettings.vSync = vSyncDropdown.value;
+    }
+    public void onMusicVolumeChange()
+    {
+        musicSource.volume = gameSettings.musicVolume = musicVolumeSlider.value;
+    }
+    public void onVideoButtonClick()
+    {
+        SaveSettings();
+    }
+    public void SaveSettings()
+    {
+        string jsonData = JsonUtility.ToJson(gameSettings, true);
+        File.WriteAllText(Application.persistentDataPath + "/gamesettings.json", jsonData);
+    }
+    public void LoadSettings()
+    {
+        gameSettings = JsonUtility.FromJson<GameSettings>(Application.persistentDataPath + "/gamesettings.json");
+
+        musicVolumeSlider.value = gameSettings.musicVolume;
+        antialisingDropdown.value = gameSettings.antialiasing;
+        vSyncDropdown.value = gameSettings.vSync;
+        textureQualityDropdown.value = gameSettings.textureQuality;
+        resolutionDropdown.value = gameSettings.resolutionIndex;
+        fullscreenToggle.isOn = gameSettings.fullscreen;
+        Screen.fullScreen = gameSettings.fullscreen;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+  
+  
+}
+
--- Assets/Menu/Scripts/quitOnClick.cs
+++ Assets/Menu/Scripts/quitOnClick.cs
@@ -7,9 +7,9 @@ public class quitOnClick : MonoBehaviour {
     public void Quit()
     {
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+        UnityEditor.EditorApplication.isPlaying = false; // Will quit to the editor if playing in the editor
 #else
-        Application.Quit ();
+        Application.Quit (); // If this is a build of the game, quit to desktop.
 #endif
     }
 

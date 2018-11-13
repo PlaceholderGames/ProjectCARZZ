25b723e1bf62ae07f7e1fb5b7e5f4188e10f1754
Jason Lavarinhas <kayloshai@gmail.com>
2018-11-13 16:46:30 +0000

    Added script files
    -LevelingSystem
    -DamageSystem

---
+++ Assets/Scripts/DamageSystem.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Add script to MSPlayerControllerFree or MSVehicleControllerFree
+public class DamageSystem : MonoBehaviour {
+    [HideInInspector]
+    public float RecievedDamage; //Players/Vehicles health
+}
---
+++ Assets/Scripts/LevelingSystem.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelingSystem : MonoBehaviour
+{
+
+    public int currentLevel = 1;
+    private int maxLevel = 20;
+    public int givenXP;
+    public int currentXP;
+    public int totalXP = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        totalExperience(currentLevel);
+    }
+
+    // Update is called once per frame
+    public void UpdateLevelingSystem()
+    {
+        if (currentXP >= totalXP)
+        {
+            if (currentXP > totalXP)
+            {
+                currentXP = totalXP - currentXP;
+                currentLevel++;
+            }
+            else
+            {
+                currentXP = 0;
+                currentLevel++;
+            }
+            totalExperience(currentLevel);
+        }
+    }
+
+    private bool totalExperience(int lvl)
+    {
+        if (lvl == 1)
+        {
+            totalXP = totalXP * lvl;
+            return true;
+        }
+        else if (lvl > 1)
+        {
+            totalXP = totalXP * lvl + (int)((float)totalXP / 2f);
+            return true;
+        }
+        else if (lvl == maxLevel + 1)
+        {
+            currentXP = totalXP;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}

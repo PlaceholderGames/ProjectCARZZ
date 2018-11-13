ac95b8ed3db21ff10729147618d8b07d5532f3fa
Sebastian Gerencser <32391856+Skeaar@users.noreply.github.com>
2018-11-13 16:50:39 +0000

    FACEPALM

--- Assets/AI/AIScripts/AICollision.cs
+++ Assets/AI/AIScripts/AICollision.cs
@@ -7,7 +7,7 @@ public class AICollision : MonoBehaviour
     private Animator anim;
     public float despawnTime = 5;
     
-    public bool gavceDamage = false;
+    public bool gaveDamage = false;
     public bool isDead = false;
     public bool hitPlayer = false;
     public bool ishit = false;

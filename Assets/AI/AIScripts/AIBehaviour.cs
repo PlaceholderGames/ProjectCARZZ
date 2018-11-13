4f0a3d71dbc97c8137688b98b38912e0371253d1
Sebastian Gerencser <32391856+Skeaar@users.noreply.github.com>
2018-11-13 16:14:34 +0000

    Added more animation and modified the script

--- Assets/AI/AIScripts/AIBehaviour.cs
+++ Assets/AI/AIScripts/AIBehaviour.cs
@@ -8,45 +8,69 @@ public class AIBehaviour : MonoBehaviour
     
     public Rigidbody rb;
     public Transform target;
-    public float moveSpeed = 5f;
+    public float moveSpeed = 0.011f;
+    public float runMoveSpeed = 0.1f;
     public float detectDistance= 200f;  //the distance in which the AI can follow the player
+    private float speed;
     float xpos, zpos;
+    private MSVehicleControllerFree vehicle;
     private Animator anim;
-    private Boolean hitPlayer = false;
+    private bool inCar = false;
+    
+    public AICollision aiCollision;
 
     private void Start()
     {
-        //anim = GetComponent<Animator>();
+        
         anim = GetComponentInChildren<Animator>();
+        vehicle = FindObjectOfType<MSVehicleControllerFree>();
     }
 
-    private void OnCollisionEnter(Collision collisionInfo)
-    {
-        if (collisionInfo.collider.tag == "Player")
-            hitPlayer = true;
-    }
+   
 
     void FixedUpdate()
-
     {
-        
+            /// Check if player is inside vehicle
+        if (vehicle.isInsideTheCar)
+            inCar = true;
+        else
+            inCar = false;
+
         xpos = (target.position.x - transform.position.x);
         zpos = (target.position.z - transform.position.z);
-        //if the player is inside the detectable distance
-        if ((Math.Abs(xpos) < detectDistance && Math.Abs(zpos) < detectDistance) && !hitPlayer)
-        {
-            anim.SetBool("isIdle", false);
-            Vector3 rot = target.transform.position - transform.position;
-            rot.y = 0;
-            Quaternion rotation = Quaternion.LookRotation(rot);
-            transform.rotation = rotation;
-            //The AI gets the position of the player and it goes towards it.
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, moveSpeed);
+        /// Checks if the hit animation is playing
+        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Zombie Reaction Hit"))
+        { ///Hit animation is not playing
+            //if the player is inside the detectable distance
+            if ((Math.Abs(xpos) < detectDistance && Math.Abs(zpos) < detectDistance) && !(aiCollision.hitPlayer))
+            {
+                anim.SetBool("isIdle", false); ///Player in car
+                if (inCar)
+                {
+                    anim.SetBool("isRunning", true); //Zombie runs
+                    speed = runMoveSpeed;
+                }
+                else
+                {
+                    anim.SetBool("isWalking", true); //Zombie walks
+                    speed = moveSpeed;
+                }
+                Vector3 rot = target.transform.position - transform.position;
+                rot.y = 0;
+                Quaternion rotation = Quaternion.LookRotation(rot);
+                transform.rotation = rotation;
+                //The AI gets the position of the player and it goes towards it.
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+            }
+            else //Resets to idle animation if they're out of the range
+            {
+                anim.SetBool("isIdle", true);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", false);
+            }
         }
         else
-        {
-            anim.SetBool("isIdle", true);
-        }
-          
+            anim.SetBool("isHit", false);
+
     }
 }
--- Assets/AI/AIScripts/AICollision.cs
+++ Assets/AI/AIScripts/AICollision.cs
@@ -3,23 +3,36 @@ using UnityEngine;
 
 public class AICollision : MonoBehaviour
 {
-    public GameObject ai;
+    
     private Animator anim;
+    public float despawnTime = 5;
+    
+    public bool gavceDamage = false;
+    public bool isDead = false;
+    public bool hitPlayer = false;
+    public bool ishit = false;
     private void Start()
     {
-        //anim = GetComponent<Animator>();
+        
         anim = GetComponentInChildren<Animator>();
+       
     }
 
     private void OnCollisionEnter(Collision collisionInfo)
     {
        if (collisionInfo.collider.tag == "Player")
         {
+            hitPlayer = true;
             anim.SetBool("isWon", true);
         }
         if (collisionInfo.collider.tag == "Vehicle")
         {
-            Destroy(ai, 5);
+            if (vehicle.KMh > 30)
+                Destroy(gameObject, despawnTime);
+            else
+            {
+                anim.SetBool("isHit", true);
+            }
         }
     }
 

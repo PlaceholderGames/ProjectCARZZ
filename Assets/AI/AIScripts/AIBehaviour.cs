bd16876b1d8b2a25c42221ae46e3d8cd2ffa5692
Jason Lavarinhas <kayloshai@gmail.com>
2018-11-29 04:09:24 +0000

    SpawnObject
    Reworking on the spawn object and ai prefabs to the map, all across the map instead of just at one definded location.

--- Assets/AI/AIScripts/AIBehaviour.cs
+++ Assets/AI/AIScripts/AIBehaviour.cs
@@ -33,41 +33,39 @@ public class AIBehaviour : MonoBehaviour
     {
         xpos = (target.position.x - transform.position.x);
         zpos = (target.position.z - transform.position.z);
-        
-        
-        
-            //if the player is inside the detectable distance
-            if ((Math.Abs(xpos) < detectDistance && Math.Abs(zpos) < detectDistance))//&& !(aiCollision.hitPlayer)
+
+
+
+        //if the player is inside the detectable distance
+        if ((Math.Abs(xpos) < detectDistance && Math.Abs(zpos) < detectDistance))//&& !(aiCollision.hitPlayer)
+        {
+            anim.SetBool("isIdle", false); ///Player in car
+            if (vehicle.isInsideTheCar)
             {
-                anim.SetBool("isIdle", false); ///Player in car
-                if (vehicle.isInsideTheCar)
-                {
-                    anim.SetBool("isWalking", false);
-                    anim.SetBool("isRunning", true); //Zombie runs
-                    speed = runMoveSpeed;
-                }
-                else
-                {
-                    anim.SetBool("isWalking", true); //Zombie walks
-                    anim.SetBool("isRunning", false);
-                    speed = moveSpeed;
-                }
-                Vector3 rot = target.transform.position - transform.position;
-                rot.y = 0;
-                Quaternion rotation = Quaternion.LookRotation(rot);
-                transform.rotation = rotation;
-                //The AI gets the position of the player and it goes towards it.
-                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
-            }
-            else //Resets to idle animation if they're out of the range
-            {
-                anim.SetBool("isIdle", true);
                 anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true); //Zombie runs
+                speed = runMoveSpeed;
+            }
+            else
+            {
+                anim.SetBool("isWalking", true); //Zombie walks
                 anim.SetBool("isRunning", false);
+                speed = moveSpeed;
+            }
+            Vector3 rot = target.transform.position - transform.position;
+            rot.y = 0;
+            Quaternion rotation = Quaternion.LookRotation(rot);
+            transform.rotation = rotation;
+            //The AI gets the position of the player and it goes towards it.
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed);
+        }
+        else //Resets to idle animation if they're out of the range
+        {
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+        }
             AIPos.x = transform.position.x;
             AIPos.z = transform.position.z;
-        }
-       
-
     }
 }
--- Assets/Scripts/NewSpawnObject.cs
+++ Assets/Scripts/NewSpawnObject.cs
@@ -5,71 +5,131 @@ using UnityEngine;
 public class NewSpawnObject : MonoBehaviour {
     public Terrain terrain;
     public int currentObjects;//current amount of objects in scene
+    public int CurrentNumberAi = 0;//number of ai at current time
     public int maxNumberObjects;//max number of objects in scene
-    public int SpawnIntervalAi;
+    public int MaxNumberAi = 5;//max amount of ai at a time
+    public float despawnTime;//time before ai is killed
+    public float SpawnIntervalAi = 5;//time it takes for new ai to spawn
+    public float SpawnIntervalcollectables = 5;//time it takes for new ai to spawn
+
+    public int DetectDistanceAi = 50;
     public GameObject[] objectToPlace;
-    private CoinSystem[] coin;
-    private FuelSystem[] gas;
-    private RepairSystem[] repair;
+    private GameObject[] moneySmall;
+    private GameObject[] moneyMedium;
+    private GameObject[] moneyLarge;
+    private GameObject[] repair;
+    private GameObject[] ai;
+
     private int terrainWidth;
     private int terrainLength;
     private int terrainPosX;
     private int terrainPosY;
     private bool isSpawning;
     
-    void Start () {
+    private AIBehaviour[] aIBehaiour;
+    private AICollision[] aICollision;
+    private MSVehicleControllerFree vehicle;
+    private MSFPSControllerFree player;
+    private Transform tempTransform;//used for switiching between player and vehicle
+    private Transform vehicleTransform;//vehicle in current scene
+    private Transform playerTransform;//player in current scene
+
+    
+    private float moveSpeed = 0.015f;//speed at which ai move
+
+    private void Instantiation()
+    {
         terrainWidth = (int)terrain.terrainData.size.x;
         terrainLength = (int)terrain.terrainData.size.z;
         terrainPosX = (int)terrain.transform.position.x;
         terrainPosY = (int)terrain.transform.position.z;
-        coin = FindObjectsOfType<CoinSystem>();
-        gas = FindObjectsOfType<FuelSystem>();
-        repair = FindObjectsOfType<RepairSystem>();
+
+        moneySmall = GameObject.FindGameObjectsWithTag("moneySmall");
+        //moneyMedium = GameObject.FindGameObjectsWithTag("moneyMedium");
+        //moneyLarge = GameObject.FindGameObjectsWithTag("moneyLarge");
+        //repair = GameObject.FindGameObjectsWithTag("repair");
+        //ai = GameObject.FindGameObjectsWithTag("ai");
+
+        //aIBehaiour = FindObjectsOfType<AIBehaviour>();
+        //aICollision = FindObjectsOfType<AICollision>();
+        //vehicle = FindObjectOfType<MSVehicleControllerFree>();
+        //player = FindObjectOfType<MSFPSControllerFree>();
+        //vehicleTransform = vehicle.transform;
+
         isSpawning = false;
+    }
+
+    void Start () {
+        Instantiation();
+    }
+    
+    private void ObjectPrefab(GameObject[] obj, int x , int y, int i)
+    {
+        for (int c = 0; c < obj.Length; c++)
+        {
+            if (obj[i].transform.position.x == x && obj[i].transform.position.z == y)
+            {
+                x = Random.Range(terrainPosX, terrainPosX + terrainWidth);
+                y = Random.Range(terrainPosY, terrainPosY + terrainLength);
+            }
+        }
+    }
+    private void SpawnAI()
+    {
+
     }
 
     public void SpawnObject()
     {
-        coin = FindObjectsOfType<CoinSystem>();
-        gas = FindObjectsOfType<FuelSystem>();
-        repair = FindObjectsOfType<RepairSystem>();
-
         int posx = Random.Range(terrainPosX, terrainPosX + terrainWidth);
         int posz = Random.Range(terrainPosY, terrainPosY + terrainLength);
         float posy = Terrain.activeTerrain.SampleHeight(new Vector3(posx, 0, posz));
-        posy = Terrain.activeTerrain.SampleHeight(new Vector3(posx, 0, posz));
 
+        moneySmall = GameObject.FindGameObjectsWithTag("moneySmall");
+        //moneyMedium = GameObject.FindGameObjectsWithTag("moneyMedium");
+        //moneyLarge = GameObject.FindGameObjectsWithTag("moneyLarge");
+        //repair = GameObject.FindGameObjectsWithTag("repair");
+        //ai = GameObject.FindGameObjectsWithTag("ai");
+
+        
 
         for (int i = 0; i < objectToPlace.Length; i++)
         {
-            GameObject newObject = (GameObject)Instantiate(objectToPlace[i], new Vector3(posx, posy + 1, posz), Quaternion.identity);
-            for(int c = 0; c < coin.Length; c++)
+            for(int l = 0; l < maxNumberObjects; l++)
             {
-                if (coin[i].transform.position.x == posx && coin[i].transform.position.z == posz)
-                {
-                    posx = Random.Range(terrainPosX, terrainPosX + terrainWidth);
-                    posz = Random.Range(terrainPosY, terrainPosY + terrainLength);
-                }
+                GameObject newObject = (GameObject)Instantiate(objectToPlace[i], new Vector3(posx, posy + 1, posz), Quaternion.identity);
+                ObjectPrefab(moneySmall, posx, posz, i);
+                currentObjects++;
             }
-            for (int g = 0; g < gas.Length; g++)
-            {
-                if (gas[i].transform.position.x == posx && gas[i].transform.position.z == posz)
-                {
-                    posx = Random.Range(terrainPosX, terrainPosX + terrainWidth);
-                    posz = Random.Range(terrainPosY, terrainPosY + terrainLength);
-                }
-            }
-            for (int r = 0; r < repair.Length; r++)
-            {
-                if (repair[i].transform.position.x == posx && repair[i].transform.position.z == posz)
-                {
-                    posx = Random.Range(terrainPosX, terrainPosX + terrainWidth);
-                    posz = Random.Range(terrainPosY, terrainPosY + terrainLength);
-                }
-            }
+            
+            //ObjectPrefab(moneyMedium, posx, posz, i);
+            //ObjectPrefab(moneyLarge, posx, posz, i);
+            //ObjectPrefab(repair, posx, posz, i);
+            
 
-            isSpawning = false;
-            currentObjects++;
+            //ObjectPrefab(ai, posx, posz, i);
+            //CurrentNumberAi++;
+            
+            //aIBehaiour = FindObjectsOfType<AIBehaviour>();
+            //aICollision = FindObjectsOfType<AICollision>();
+
+            //if (vehicle.isInsideTheCar == false)
+            //    playerTransform = player.transform;
+            //else
+            //{
+            //    tempTransform = playerTransform;
+            //    playerTransform = vehicleTransform;
+            //}
+
+            //for (int k = 0; k < aIBehaiour.Length; k++)
+            //{
+            //    aIBehaiour[k].target = playerTransform;
+            //    aIBehaiour[k].moveSpeed = moveSpeed;
+            //    aIBehaiour[k].detectDistance = DetectDistanceAi;
+            //}
+
+            //for (int k = 0; k < aICollision.Length; k++)
+            //    aICollision[k].despawnTime = despawnTime;
         }
     }
     // Update is called once per frame
@@ -77,7 +137,7 @@ public class NewSpawnObject : MonoBehaviour {
     {
         if (!isSpawning && currentObjects < maxNumberObjects)
         {
-            Invoke("SpawnObject", SpawnIntervalAi);
+            Invoke("SpawnObject", SpawnIntervalcollectables);
             isSpawning = true;
         }
     }

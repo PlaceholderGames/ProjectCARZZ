08582e2121c2b833cd73f9518108bfc69b6713d1
DewyHealy <dewyhealy@yahoo.co.uk>
2019-01-29 02:54:25 +0000

    Safe Zone
    Added a lot of models for use in levels
    
    created the "safe zone" garage
    in process of making a safe zone prefab
    already done:
    fences around safe zone
    gate with opening doors
    garage
    
    to do: props, interactivity etc
    
    Added a script that attaches to objects in the world which will make a sound when you crash into them such as a fence or a rock.
    
    added fence hit sounds, metal gate hitting sounds
    
    need to find some more "impact" sounds
    
    created a new level to test out the free "EasyRoads3D" addon that can be used to make roads for the player to drive on. it's a bit -redacted- but it will work for what we need
    
    currently on the hunt for some more environmental props and things to clutter the world to make it interesting
    
    also DO NOT try to merge this commit to main as I haven't deleted all the irrelevant files yet

---
+++ Assets/Models/Safe Zone/OpenGate.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour {
+
+
+    private List<Animator> animlist;
+    public GameObject[] Elements;
+
+    void Start()
+    {
+
+        animlist = new List<Animator>();
+        for (int i = 0; i < Elements.Length; i++)
+        {
+            animlist.Add(Elements[i].GetComponent<Animator>());
+        }
+    }
+
+    void OpenDoors()
+    {
+        foreach (Animator anim in animlist)
+        {
+            anim.SetBool("open", true);
+        }
+    }
+
+    void CloseDoors()
+    {
+        foreach (Animator anim in animlist)
+        {
+            anim.SetBool("open", false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Vehicle2" || other.tag == "Vehicle3")
+        {
+            Debug.Log("Gate should be opening");
+            foreach (Animator anim in animlist)
+            {
+                anim.SetBool("open", true);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Vehicle2" || other.tag == "Vehicle3")
+        {
+            Debug.Log("Gate should be closing");
+            foreach (Animator anim in animlist)
+            {
+                anim.SetBool("open", false);
+            }
+        }
+    }
+}
---
+++ Assets/Models/SoundOnHit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundOnHit : MonoBehaviour
+{
+    public AudioMixerGroup mainMixer;
+    public AudioClip[] clips;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void PlayRandomSound()
+    {
+        Debug.Log("Collided with an object that makes a noise");
+        int randomClip = Random.Range(0, clips.Length);
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.outputAudioMixerGroup = mainMixer;
+        audioSource.clip = clips[randomClip];
+        audioSource.Play();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Vehicle2" || collision.collider.tag == "Vehicle3")
+        {
+            PlayRandomSound();
+        }
+    }
+
+}

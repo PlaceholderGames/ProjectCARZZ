564a81dc2ccc459671f7c34be49e1c4e2d2aa8f6
DewyHealy <dewyhealy@yahoo.co.uk>
2019-02-12 04:16:57 +0000

    calm before the merge
    added interactive or automatic door opening script
    
    created animations for doors
    created a prefab for safe zone
    ironing out some bugs with some other scripts
    
    almost ready to merge. need to take out any conflicting scripts and using this commit as a fallback in case it doesn't work out

---
+++ Assets/Audio/Scripts and Mixer/SoundOnAwake.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundOnAwake : MonoBehaviour {
+
+    [Header("Audio setup")]
+    public AudioMixerGroup mainMixer;
+    public AudioClip[] clips;
+    [Space(2, order = 0)]
+    [Header("Time Delay")]
+    public float timeVal;
+    public bool randomTime;
+    [Space(2, order = 0)]
+    [Header("Volume properties")]
+    [Tooltip("If a random volume is desired, press the button and specify both the lowest and highest volumes, otherwise the highest volume is used.")]
+    public bool randomVolume;
+    public float lowestVol;
+    public float highestVol;
+
+    void PlayRandomSound()
+    {
+        Debug.Log("Collided with an object that makes a noise");
+        int randomClip = Random.Range(0, clips.Length);
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.outputAudioMixerGroup = mainMixer;
+        audioSource.clip = clips[randomClip];
+        if (randomVolume == true)
+        {
+            audioSource.volume = Random.Range(lowestVol, highestVol);
+        }
+        else
+        {
+            audioSource.volume = highestVol;
+        }
+        audioSource.Play();
+        Debug.Log(audioSource.volume);
+    }
+
+    private void Awake()
+    {
+        if (randomTime != true)
+        {
+            Invoke("PlayRandomSound", timeVal);
+        }
+        else
+            Invoke("PlayRandomSound", Random.Range(0, timeVal));
+    }
+
+}
---
+++ Assets/Models/Safe Zone/Gate/OpenGate.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGate : MonoBehaviour {
+
+    private List<Animator> animlist;
+    public GameObject[] Elements;
+    public bool interactive; //if we want door to open on player command
+    private bool insideTrigger; //check for input only when inside trigger
+    private bool doorOpen; //check if doors are open
+    private bool active; //check if doors are opening
+
+    void Start()
+    {
+        animlist = new List<Animator>();
+        for (int i = 0; i < Elements.Length; i++)
+        {
+            animlist.Add(Elements[i].GetComponent<Animator>());
+        }
+    }
+
+    void doorConflict()
+    {
+        active = false;
+    }
+
+    void OpenDoors()
+    {
+        active = true;
+        doorOpen = true;
+        foreach (Animator anim in animlist)
+        {
+            anim.SetBool("open", true);
+        }
+        Invoke("doorConflict", 1);
+    }
+
+    void CloseDoors()
+    {
+        active = true;
+        doorOpen = false;
+        foreach (Animator anim in animlist)
+        {
+            anim.SetBool("open", false);
+        }
+        Invoke("doorConflict", 1);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Vehicle2" || other.tag == "Vehicle3" || other.tag == "Player")
+        {
+            if (!interactive)
+            {
+                OpenDoors();
+            }
+            else
+            {
+                insideTrigger = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Vehicle2" || other.tag == "Vehicle3" || other.tag == "Player")
+        {
+            if (!interactive)
+            {
+                CloseDoors();
+            }
+            else
+            {
+                insideTrigger = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (interactive == true)
+        {
+            if (insideTrigger == true)
+            {
+                if (Input.GetKeyDown(KeyCode.Return) && doorOpen == false && active == false)
+                {
+                    OpenDoors();
+                }
+                if (Input.GetKeyDown(KeyCode.Return) && doorOpen == true && active == false)
+                {
+                    CloseDoors();
+                }
+            }
+        }
+    }
+}

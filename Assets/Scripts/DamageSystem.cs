1c908db22e7604c34f198a47c015128a83444df7
DewyHealy <dewyhealy@yahoo.co.uk>
2019-02-19 02:14:21 +0000

    I am ready
    to... merge.
    Let's not put it off any longer.

--- Assets/Scripts/DamageSystem.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-//Add script to MSPlayerControllerFree or MSVehicleControllerFree
-public class DamageSystem : MonoBehaviour {
-    [HideInInspector]
-    public float RecievedDamage; //Players/Vehicles health
-}
